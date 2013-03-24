8ee064e82e9a5aa5b6c6219902a30541d71c6c9a
Thomas Levine <occurrence@thomaslevine.com>
2013-03-24 14:59:24 -0400

    blah

--- src/Main.hs
+++ src/Main.hs
@@ -4,22 +4,28 @@ import Snap.Util.FileServe
 -- import Snap.Extras.CoreUtils
 -- import Snap.Extras.TextUtils
 import Control.Lens
-import Data.Text
-import Data.Text.Encoding
+-- import Data.Text
+-- import Data.Text.Encoding
 import Data.Monoid
 import Heist.Interpreted
+
+data TT = TT { _heist :: Snaplet (Heist TT)
+             }
+makeLenses ''TT
+
+instance HasHeist TT where
+  heistLens = subSnaplet heist
 
 memoiseInit :: SnapletInit TT TT
 memoiseInit = makeSnaplet "Trailer Tracker" "Track inhabitable FEMA trailers" Nothing $ do
   h <- nestSnaplet "heist" heist $ heistInit "templates"
-  -- modifyHeistState $ bindAttributeSplices [("main-textbox", 
-mainTextboxAttributeSplice)]
+  -- modifyHeistState $ bindAttributeSplices [("main-textbox", mainTextboxAttributeSplice)]
   addRoutes [ ("images", serveDirectory "static/images")
             , ("stylesheets", serveDirectory "static/stylesheets")
             , ("", indexHandler)
             ]
-  return $ Memoise { _heist = h
-                   }
+  return $ TT { _heist = h
+              }
 
 main :: IO ()
 main = do
