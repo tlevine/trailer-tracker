3a546a2ffe884b56a6f88eec038d8c19acc4a1e3
Thomas Levine <occurrence@thomaslevine.com>
2013-03-27 02:17:14 -0400

    namespace

--- templates.hs
+++ templates.hs
@@ -2,7 +2,6 @@
 
 -- http://mschade.me/post/34014291111/html-conditional-comments-with-blaze-html
 import           Text.Blaze.Internal
-import           Text.Blaze.Html5.Attributes
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import qualified Data.ByteString.Char8 as C8
@@ -17,37 +16,37 @@ basePage = H.docTypeHtml $ do
     H.head $ do
         --  Basic Page Needs
         --   ================================================== 
-        H.meta ! charset "utf-8"
+        H.meta ! A.charset "utf-8"
         H.title "Trailer Tracker"
-        H.meta ! name "description" ! content "Informing and connecting people around the resale of over 150,000 potentially hazardous FEMA trailers"
-        H.meta ! name "author" ! content "Nick Shapiro and Thomas Levine"
+        H.meta ! A.name "description" ! A.content "Informing and connecting people around the resale of over 150,000 potentially hazardous FEMA trailers"
+        H.meta ! A.name "author" ! A.content "Nick Shapiro and Thomas Levine"
         --  Mobile Specific H.metas
         --   ================================================== 
-        H.meta ! name "viewport" ! content "width=device-width, initial-scale=1, maximum-scale=1"
+        H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1, maximum-scale=1"
         --  CSS
         --   ================================================== 
-        H.link ! rel "stylesheet" ! href "/stylesheets/base.css"
-        H.link ! rel "stylesheet" ! href "/stylesheets/skeleton.css"
-        H.link ! rel "stylesheet" ! href "/stylesheets/layout.css"
+        H.link ! A.rel "stylesheet" ! A.href "/stylesheets/base.css"
+        H.link ! A.rel "stylesheet" ! A.href "/stylesheets/skeleton.css"
+        H.link ! A.rel "stylesheet" ! A.href "/stylesheets/layout.css"
         --  Favicons
         -- 	================================================== 
-        H.link ! rel "shortcut icon" ! href "/images/favicon.ico"
-        H.link ! rel "apple-touch-icon" ! href "/images/apple-touch-icon.png"
-        H.link ! rel "apple-touch-icon" ! sizes "72x72" ! href "/images/apple-touch-icon-72x72.png"
-        H.link ! rel "apple-touch-icon" ! sizes "114x114" ! href "/images/apple-touch-icon-114x114.png"
+        H.link ! A.rel "shortcut icon" ! A.href "/images/favicon.ico"
+        H.link ! A.rel "apple-touch-icon" ! A.href "/images/apple-touch-icon.png"
+        H.link ! A.rel "apple-touch-icon" ! A.sizes "72x72" ! A.href "/images/apple-touch-icon-72x72.png"
+        H.link ! A.rel "apple-touch-icon" ! A.sizes "114x114" ! A.href "/images/apple-touch-icon-114x114.png"
     H.body $ do
         --  Primary Page Layout
         -- 	================================================== 
-        --  Delete everything in this .container and get started on your own site! 
-        H.div ! class_ "container" $ do
-            H.header ! class_ "sixteen columns" $ do
-                H.a ! href "/" $ H.h1 ! class_ "remove-bottom" $ "Trailer Tracker"
-                H.form ! action "/logout" ! method "post" ! class_ "account" $ H.button ! type_ "submit" $ "Log out"
-                H.a ! href "/login" ! class_ "account button" $ "Log in"
-                H.a ! href "/me" ! class_ "account button" $ "Account"
+        --  Delete everything in this .container and get started on your own site! A.
+        H.div ! A.class_ "container" $ do
+            H.header ! A.class_ "sixteen columns" $ do
+                H.a ! A.href "/" $ H.h1 ! A.class_ "remove-bottom" $ "Trailer Tracker"
+                H.form ! A.action "/logout" ! A.method "post" ! A.class_ "account" $ H.button ! A.type_ "submit" $ "Log out"
+                H.a ! A.href "/login" ! A.class_ "account button" $ "Log in"
+                H.a ! A.href "/me" ! A.class_ "account button" $ "Account"
                 H.hr
             H.div ! A.id "body" $ mempty
-            H.footer ! class_ "sixteen columns" $ do
+            H.footer ! A.class_ "sixteen columns" $ do
                 H.hr
                 H.p "Read more."
         --  container 
