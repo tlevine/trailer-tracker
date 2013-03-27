788486246321ca39ac6b0d135dbc1a5325d66d17
Thomas Levine <occurrence@thomaslevine.com>
2013-03-27 03:18:39 -0400

    static

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Monad
-import Happstack.Server    (Response, ServerPart, dir, mapServerPartT, look, nullConf, ok, simpleHTTP, toResponse, path)
+import Control.Monad       (msum)
+import Happstack.Server -- (Response, ServerPart, dir, mapServerPartT, look, nullConf, ok, simpleHTTP, toResponse, path)
 
 import qualified Data.UUID as U
 import Templates
 
-{-
-mai1 :: IO ()
-mai1 = simpleHTTP nullConf $ msum [ dir "trailers"    $ path $ \uuid -> ok "Trailer observation "   ++ uuid
-                                  , dir "symptoms"    $ path $ \uuid -> ok "Symptom questionnaire " ++ uuid
---                                , dir "images"      $ serveDirectory "static/images"
---                                , dir "stylesheets" $ serveDirectory "static/stylesheets"
-                                  ]
--}
-
-simpleIndex :: ServerPart Response
-simpleIndex = ok $ toResponse $ index U.nil
-
 main :: IO ()
-main = simpleHTTP nullConf $ simpleIndex
+main = simpleHTTP nullConf $ msum [ method GET >> nullDir >> (ok $ toResponse $ index U.nil)
+                                  , dir "images"      $ serveDirectory DisableBrowsing [] "../static/images"
+                                  , dir "stylesheets" $ serveDirectory DisableBrowsing [] "../static/stylesheets"
+--                                , dir "symptoms"    $ path $ \uuid -> ok $ "Symptom questionnaire " ++ uuid
+--                                , dir "trailers"    $ path $ \uuid -> ok $ "Trailer observation "   ++ uuid
+                                  ]
